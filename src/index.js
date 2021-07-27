69afb84976c739b0cc08b87e82316e348d62bc1b
김민기 <mingikim@gimmingiui-MacBookAir.local>
2021-07-27 22:36:34 +0900

    style : Global-style, style-reset 적용

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CardList from "./CardList";
+import GlobalStyle from "./style/GlobalStyle";
 
-ReactDOM.render(<CardList />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <CardList />
+  </>,
+  document.getElementById("root")
+);
